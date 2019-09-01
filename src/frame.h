8e3b61484e85567bf5603ae8d9fc571cc3ffd083
Donnerbrenn <thunder_burn@gmx.de>
2019-09-01 19:54:34 +0200

    performance improvemts

--- src/frame.h
+++ src/frame.h
@@ -8,7 +8,7 @@ class frame : public element
 public:
     frame():element(){}
     frame(int x, int y, int w, int h, sf::String label="",sf::Color bgColor=sf::Color(128,128,128), sf::Color fgColor=sf::Color::Black):
-    element(x,y,w,h,"bitmaps/frame.png",frame_type)
+    element(x,y,w,h,"",frame_type)
     {
         this->fgColor=fgColor;
         this->bgColor=bgColor;
