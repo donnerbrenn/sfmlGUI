49d3f7cbaacbd78f1ac58d066704a646009a5440
Donnerbrenn <thunder_burn@gmx.de>
2019-09-01 19:53:22 +0200

    performance improvements

--- src/frame.cpp
+++ src/frame.cpp
@@ -2,19 +2,8 @@
 
 void frame::draw(sf::RenderWindow *window)
 {
-    sf::RectangleShape border;
-    sf::RectangleShape back;
-    border.setSize(sf::Vector2f(width,height));
-    border.setFillColor(fgColor);
-    border.setPosition(posX,posY);
     window->draw(border);
-
-    back.setSize(sf::Vector2f(width-4,height-4));
-    back.setFillColor(bgColor);
-    back.setPosition(posX+2,posY+2);
     window->draw(back);
-
-
 
     sf::Sprite image;
     image.setTexture(canvas.getTexture());
--- src/frame.h
+++ src/frame.h
@@ -13,10 +13,20 @@ public:
         this->fgColor=fgColor;
         this->bgColor=bgColor;
         initCanvas(w,h);
+
+        border.setSize(sf::Vector2f(width,height));
+        border.setFillColor(fgColor);
+        border.setPosition(posX,posY);
+
+        back.setSize(sf::Vector2f(width-4,height-4));
+        back.setFillColor(bgColor);
+        back.setPosition(posX+2,posY+2);
     }
     virtual void draw(sf::RenderWindow *window);
 
 private:
     sf::Color bgColor;
     sf::Color fgColor;
+    sf::RectangleShape border;
+    sf::RectangleShape back;
 };
